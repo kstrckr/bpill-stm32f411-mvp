8ddca716675c5c5d0583892a3b5e364350ea307b
Kurt Strecker <ksphotopro@gmail.com>
2021-07-22 23:16:03 -0400

    working spi with eeprom

--- src/main.rs
+++ src/main.rs
@@ -9,8 +9,12 @@ use cortex_m_rt::entry;
 use stm32f4xx_hal as hal;
 use hal::spi::Spi;
 use hal::spi::{Mode, Phase, Polarity};
+use hal::rcc::{Clocks, Rcc};
 use crate::hal::{prelude::*, stm32};
 
+fn setup_clocks(rcc: Rcc) -> Clocks {
+    return rcc.cfgr.hclk(84.mhz()).sysclk(84.mhz()).pclk1(36.mhz()).pclk2(36.mhz()).freeze();
+}
 #[entry]
 fn main() -> ! {
 
@@ -20,17 +24,16 @@ fn main() -> ! {
     ) {
         // Set up the system clock.
         let rcc = dp.RCC.constrain();
-        let clocks = rcc.cfgr.freeze();
+        let clocks = setup_clocks(rcc);
         let mut delay = hal::delay::Delay::new(cp.SYST, clocks);
         let gpioa = dp.GPIOA.split();
-        let (mut cs, clk, mosi) = (
+        let (mut cs, clk, miso, mosi) = (
             gpioa.pa4.into_push_pull_output(),
             gpioa.pa5.into_alternate_af5(),
+            gpioa.pa6.into_alternate_af5(),
             gpioa.pa7.into_alternate_af5(),
         );
 
-        let gpiob = dp.GPIOB.split();
-        let miso = gpiob.pb4.into_alternate_af5();
 
         let mut spi = Spi::spi1(
             dp.SPI1,
@@ -44,12 +47,23 @@ fn main() -> ! {
         );
 
         // CS HIGH = Standby mode
-        cs.set_high();
+        cs.set_high().unwrap();
         delay.delay_ms(100_u32);
-        cs.set_low();
+        cs.set_low().unwrap();
 
-        let mfgId = spi.transfer(&mut [0x9F]);
+
+        // let ready = spi.is_txe();
+        spi.write(&[0x7C]);
+
+        cs.set_high().unwrap();
+        delay.delay_ms(100_u32);
+        cs.set_low().unwrap();
+
+        let cmd = &mut [0x9F, 0, 0, 0, 0, 0];
+        let mfgId = spi.transfer(cmd).ok();
+        let read = spi.read();
         cs.set_high();
+
         loop {
             continue;
         }
