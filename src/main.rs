cccfe0446767d556125866af53926d9be908215d
Kurt Strecker <ksphotopro@gmail.com>
2021-08-25 23:04:27 -0400

    brought up to date with general changes since i2c

--- src/main.rs
+++ src/main.rs
@@ -5,7 +5,7 @@
 
 // Halt on panic
 use panic_halt as _;
-extern crate stsafe_rs;
+// extern crate stsafe_rs;
 
 use cortex_m_rt::entry;
 use stm32f4xx_hal as hal;
@@ -25,22 +25,13 @@ fn main() -> ! {
         let rcc = dp.RCC.constrain();
         let clocks = rcc.cfgr.use_hse(25.mhz()).freeze();
         let mut delay = hal::delay::Delay::new(cp.SYST, clocks);
-        let gpiob = dp.GPIOB.split();
-        let (scl, sda) = (
-            gpiob.pb6.into_alternate_af4_open_drain(),
-            gpiob.pb7.into_alternate_af4_open_drain(),
-        );
+        let gpiob = dp.GPIOB.split();        let mut trigger = gpiob.pb10.into_push_pull_output();
 
-        let i2c = I2c::new(dp.I2C1, (scl, sda), KiloHertz(100), clocks);
-
-        let mut stsafe = stsafe_rs::StsafeA110::new(i2c);
-        let res_ok: bool = stsafe.reset(&mut delay);
-
-        delay.delay_ms(35_u8);
-
-        let mut buff: [u8; 8] = [0; 8];
-        let random_bytes = stsafe.get_random_bytes(&mut delay, &mut buff[..]);
-
+        trigger.set_low().ok();
+        delay.delay_ms(250_u16);
+        trigger.set_high().ok();
+        delay.delay_ms(250_u16);
+        trigger.set_low().ok();
         loop {
             continue;
         }
